2cd6978128258df5bba2d315e39342e00e425add
Jacob <je03@live.se>
2024-12-19 17:19:32 +0100

    visually formatted main a bit and removed some comments

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,33 +1,6 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*b
-*   Copyright (c) 2013-2022 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 #include "game.h"
 
-
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {    
     // Initialization
@@ -44,59 +17,30 @@ int main(void)
     game.resources = resources;
     game.Launch();
 
-    
+    InitAudioDevice(); //TODO: Do we have audio?
     //--------------------------------------------------------------------------------------
-
-    InitAudioDevice();
-
-    auto sound = LoadSound("./hitHurt.ogg");
-    
-
-
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        //if (IsKeyPressed(KEY_SPACE))
-        //{
-        //    PlaySound(sound);
-        //}
-
-        //if (IsKeyPressed(KEY_BACKSPACE))
-        //{
-        //    StopSound(sound);
-        //}
-
         game.Update();
       
-
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
         ClearBackground(BLACK);
-
-       
-
         game.Render();
 
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
-
     CloseAudioDevice();
     
     // De-Initialization
     //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
-
-    std::string filename = "level.txt";  
-
+    std::string filename = "level.txt";  //TODO: WHAT?
     return 0;
 }
