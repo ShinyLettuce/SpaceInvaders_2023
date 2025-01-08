c9f1313325b14805f4c0f677fac923551ba00612
Jacob <je03@live.se>
2025-01-08 10:58:13 +0100

    removed commented code in alien.cpp

--- Source/alien.cpp
+++ Source/alien.cpp
@@ -28,11 +28,6 @@ void Alien::Update()
 
 void Alien::Render(const MyTexture2D& texture)
 {
-	//DrawRectangle((int)position.x - 25, (int)position.y, 30, 30, RED);
-	//DrawCircle((int)position.x, (int)position.y, radius, GREEN);
-
-
-
 	DrawTexturePro(texture.get(),
 		{
 			0,
