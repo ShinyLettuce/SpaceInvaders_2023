cd8a05ade683991860067574075ff403eed4303b
Jacob <je03@live.se>
2025-01-08 15:28:22 +0100

    Removed comments from projectile.h

--- Source/projectile.h
+++ Source/projectile.h
@@ -6,13 +6,11 @@
 struct Projectile
 {
 public:
-	// INITIALIZE PROJECTILE WHILE DEFINING IF ITS PLAYER OR ENEMY 
 	Vector2 position = { 0,0 };
 	int speed = 15;
 	bool active = true;
 	EntityType type = {};
 
-	// LINE WILL UPDATE WITH POSITION FOR CALCULATIONS
 	Vector2 lineStart = { 0, 0 };
 	Vector2 lineEnd = { 0, 0 };
 
